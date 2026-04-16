263e0dd783e8a1c10bb95c6593b746f798b18967
penyelamatbumi <inihanip7@gmail.com>
2026-04-16 11:20:01 +0700

    menambahkan display

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -175,6 +175,24 @@ void bubbleSortArray()
     } while (pass <= nPanjang - 1);
 }
 
+void display()
+{
+    cout << "\n=============================\n";
+    cout << " Element Array Setelah Diurutkan (Asc)\n";
+    cout << "\n=============================\n";
+
+    for (int j = 0; j < nPanjang; j++)
+    {
+        cout << element[j];
+        if (j < nPanjang - 1)
+        {
+            cout << " -> ";
+        }
+    }
+    cout << endl;
+}
+
+
 
 
 
