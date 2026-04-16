b0d81b70b377867e10e9a2067d9f31299319afdb
penyelamatbumi <inihanip7@gmail.com>
2026-04-16 11:19:34 +0700

    menambahkan bubblesortarray

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -157,6 +157,25 @@ for (int i = 0; i < nPanjang; i++)
     }
 }
 
+void bubbleSortArray()
+{
+    int pass = 1;
+    do
+    {
+        for (int j = 0; j <= nPanjang - 1 - pass; j++)
+        {
+            if (element[j] > element[j + 1])
+            {
+                int temp = element[j];
+                element[j] = element[j + 1];
+                element[j + 1] = temp;
+            }
+        } 
+        pass++;
+    } while (pass <= nPanjang - 1);
+}
+
+
 
 
 
